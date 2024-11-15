967eb2bed4c869462831e8dd76d751c0c7023728
P-MCC <mehmethocaj5@gmail.com>
2024-11-15 16:49:55 +0200

    Model

--- Model.py
+++ Model.py
@@ -1,44 +1,4 @@
 import torch.nn as nn
-import torch.nn.functional as F
-
-class SimpleCNN(nn.Module):
-    def __init__(self, num_classes):
-        """
-        Initializes the SimpleCNN class for image classification.
-
-        Args:
-            num_classes (int): The number of output classes for classification.
-        """
-        super(SimpleCNN, self).__init__()
-
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(32 * 32 * 32, 128)  # Adjust dimensions based on your architecture
-        self.fc2 = nn.Linear(128, num_classes)
-        self.dropout = nn.Dropout(0.5)  # Dropout rate of 50%
-
-    def forward(self, x):
-        """
-        Defines the forward pass of the network.
-
-        Args:
-            x (torch.Tensor): The input tensor.
-
-        Returns:
-            torch.Tensor: The output tensor after passing through the network.
-        """
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2)
-        x = F.relu(self.conv3(x))
-        x = F.max_pool2d(x, 2)
-        x = x.view(x.size(0), -1)
-        x = self.dropout(F.relu(self.fc1(x)))  # Apply dropout
-        x = self.fc2(x)
-        return x
-    
 class AlexNet(nn.Module):
         def __init__(self, num_classes=10):
             super(AlexNet, self).__init__()
